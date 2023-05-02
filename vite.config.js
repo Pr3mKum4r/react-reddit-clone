b1a2825ef20d6fef3eda1a8d55c55f082dae3f14
Pr3mKum4r <premkumar.singh2021@vitstudent.ac.in>
2023-05-02 13:34:49 +0530

    first-commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-reddit-clone/dist/',
+  base: '/react-reddit-clone/',
   plugins: [react()],
 })
